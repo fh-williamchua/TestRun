9d19f7368d22fda017bd38eb94026dd97faf0c39
fh-williamchua <william.chua@fleischhacker.biz>
2025-09-29 16:41:50 +0800

    Test Errors

--- TestA/Form1.vb
+++ TestA/Form1.vb
@@ -4,6 +4,8 @@
     Private Sub btnConcat_Click(sender As Object, e As EventArgs) Handles btnConcat.Click
         _counter += 1
         txtResult.Text = txtResult.Text & _counter.ToString()
+
+        Console.WriteLin("Test")
     End Sub
 
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
@@ -11,5 +13,11 @@
 
         Dim sTest As String
         txtResult.Text = sTest
+
+        'Infinite loop
+        While True
+            Dim i As Integer = 0
+            i += 1
+        End While
     End Sub
 End Class
